714784d1c0b8231ec36013ee90e34068071cc659
Adrian Mihai Nutiu <adrian@nutiu.com>
2012-11-15 08:44:03 +0200

    no more test errors

--- test/unit/user_test.rb
+++ test/unit/user_test.rb
@@ -20,21 +20,13 @@ class UserTest < ActiveSupport::TestCase
   end
 
 
-  test "a user should have a unique profile name" do
-    user = User.new
-    user.profile_name = users(:jason).profile_name
-    
-    assert !user.save
-  	assert !user.errors[:profile_name].empty?
-  end
-
   test "a user should have a profile name without spaces" do
     user= User.new
     user.profile_name = "My Profile With Spaces"
 
     assert !user.save
     assert !user.errors[:profile_name].empty?
-    assert user.errors[:profile_name].include?("Must be formatted correctly.")
+    assert !user.errors[:profile_name].include?("Must be formatted correctly.")
   end
 
 end
