c8bae46a3f87c6657680d817e09b597cb988bd8d
Adrian Mihai Nutiu <adrian@nutiu.com>
2012-11-15 13:04:17 +0200

    Update profile page and added scope

--- config/routes.rb
+++ config/routes.rb
@@ -13,6 +13,7 @@ Treebook::Application.routes.draw do
     get 'logout', to: 'devise/sessions#destroy', as: :logout
   end
 
+  get '/:id', to: 'profiles#show'
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
